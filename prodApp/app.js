27857d9b9e712e527b31baeecef85390684920ad
Guilherme <guilherme.gfaria01@outlook.com>
2022-05-21 00:54:07 -0300

    Criando .gitignore

--- prodApp/app.js
+++ prodApp/app.js
@@ -1,52 +1,55 @@
-const createError = require('http-errors');
-const path = require('path');
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const createError = require('http-errors')
+const path = require('path')
+const dotenv = require('dotenv')
+const express = require('express')
+const mongoose = require('mongoose')
+const cookieParser = require('cookie-parser')
+const logger = require('morgan')
 
-const indexRouter = require('./routes/index');
-const productsRouter = require('./routes/produtos');
+const indexRouter = require('./routes/index')
+const productsRouter = require('./routes/produtos')
 
-const app = express();
+const app = express()
 
-const mongoose = require('mongoose');
-const mongoDB = 'mongodb+srv://dbUser:dbPscripts@cluster-0.hacc5.mongodb.net/prog_scripts?retryWrites=true&w=majority';
+dotenv.config()
 
-mongoose.connect(mongoDB, {
-  useNewUrlParser: true ,
+const mongodbLinkConnection = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_CLUSTER_NAME}.hacc5.mongodb.net/${process.env.DB_COLLECTION_NAME}?retryWrites=true&w=majority`
+
+mongoose.connect(mongodbLinkConnection, {
+  useNewUrlParser: true,
   useUnifiedTopology: true
-});
+})
 
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDBconnection error:'));
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'MongoDBconnection error:'))
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/', indexRouter);
-app.use('/produtos', productsRouter);
+app.use('/', indexRouter)
+app.use('/produtos', productsRouter)
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+app.use(function (req, res, next) {
+  next(createError(404))
+})
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+  res.status(err.status || 500)
+  res.render('error')
+})
 
-module.exports = app;
+module.exports = app
